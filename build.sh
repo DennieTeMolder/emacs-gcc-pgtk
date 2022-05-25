50183af0d83dc3cd9ddad82d14a209dfd72e3b71
Dennie te Molder <dennie503@live.nl>
2022-05-25 22:17:11 +0200

    Alias docker to self

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker build --no-cache -t konstare/emacs-gcc-pgtk .
-id=$(docker create konstare/emacs-gcc-pgtk)
+docker build --no-cache -t dennietm/emacs-gcc-pgtk:latest .
+id=$(docker create dennietm/emacs-gcc-pgtk)
 docker cp $id:/opt/deploy .
